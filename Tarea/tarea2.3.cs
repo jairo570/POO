03a508bdd3126331069c385c6b82a488c97406f3
jairo570 <47792507+jairo570@users.noreply.github.com>
2019-04-05 20:32:44 -0700

    Add files via upload

---
+++ Tarea/tarea2.3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace _2._3this
+{
+    class Dino
+    {
+        protected string n;
+
+        public Dino(string n)
+        {
+            this.n = n;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("El " + n + " es un carnivoro");
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Dino d = new Dino("Velociraptor");
+            d.Imprimir();
+        }
+    }
+}
