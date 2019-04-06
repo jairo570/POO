d800df32073185c3a9a210eb0c6489c0f4797609
jairo570 <47792507+jairo570@users.noreply.github.com>
2019-04-05 19:37:18 -0700

    Add files via upload

---
+++ Tarea/tarea 3.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace tarea_3
+{
+    class Vector2d
+    {
+        public int x, y;
+        public Vector2d(int x, int y)
+        {
+            this.x = x; this.y = y;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0},{1}", x, y);
+        }
+    }
+    abstract class Figura
+    {
+        public Vector2d position;
+        public string fill, border;
+
+        //Constructor por defecto 
+        public Figura() : this(new Vector2d(100, 100))
+        {
+
+        }
+        //constructor de figura
+        public Figura(Vector2d pos)
+        {
+            position = pos;
+            fill = "white";
+            border = "black";
+        }
+
+        public abstract void Dibuja();
+    }
+
+    class Circulo : Figura
+    {
+        private int radio;
+        public Circulo(Vector2d pos, int radio) : base(pos)
+        {
+            this.radio = radio;
+        }
+        public Circulo() : base()
+        {
+            this.radio = 10;
+        }
+
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibuja un circulo en {0} de color {1}", position, fill);
+        }
+    }
+
+    class Rectangulo : Figura
+    {
+
+        public Rectangulo(Vector2d pos) : base(pos)
+        {
+
+        }
+        public Rectangulo() : base()
+        {
+
+        }
+
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibuja un Rectangulo en {0} de color {1}", position, fill);
+        }
+    }
+
+    class Cuadrado : Figura
+    {
+        public Cuadrado(Vector2d pos) : base(pos)
+        {
+
+        }
+        public Cuadrado() : base()
+        {
+
+        }
+
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibuja un Cuadrado en {0} de color {1}", position, fill);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Figura> figuras = new List<Figura>();
+            figuras.Add(new Circulo());
+            figuras.Add(new Rectangulo(new Vector2d(200, 100)));
+            figuras.Add(new Cuadrado(new Vector2d(200, 200)));
+            foreach (Figura f in figuras)
+                f.Dibuja();
+            Console.ReadKey();
+
+        }
+    }
+}
+
