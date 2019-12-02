dff3fedae77d3b05fa23f8791bcd42a6628b0aa2
jairo570 <47792507+jairo570@users.noreply.github.com>
2019-12-01 21:20:30 -0800

    Add files via upload

---
+++ domino/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Domino
+{
+    class Ficha{
+        private readonly int ficha1;
+        private readonly int ficha2;
+        public Ficha(int superior, int inferior){
+            ficha1 = superior;
+            ficha2 = inferior;
+        }
+        public static int operator +(Ficha x, Ficha y)
+        => x.ficha1 + x.ficha2 + y.ficha1 + y.ficha2;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ficha x= new Ficha (1,0);
+            
+            Ficha y= new Ficha (4,2);
+            Console.WriteLine(x + y);
+        }
+    }
+}
